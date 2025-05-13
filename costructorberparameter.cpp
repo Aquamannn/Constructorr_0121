9ab4de1e3cc846c5eea9a6d6acfc402a5ed1d778
Lenovo <9.3razzinayyuri@gmail.com>
2025-05-13 18:54:01 +0700

    devinisi constructor dengan parameter

--- costructorberparameter.cpp
+++ costructorberparameter.cpp
@@ -8,3 +8,11 @@ class mahasiswa {
     mahasiswa(int nim, string nama); 
 };
 
+//devinisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Construktor Dengan parameter terpangil" << endl;
+    cout << "Nim : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+
