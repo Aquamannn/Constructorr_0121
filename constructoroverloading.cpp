b8449d83fc0e7d488f6b7a71600259389ad04727
Lenovo <9.3razzinayyuri@gmail.com>
2025-05-13 19:16:31 +0700

    memanggil semua variable dan parameter

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -26,3 +26,16 @@ mahasiswa::mahasiswa(int iNim, string iNama){
     nim = iNim;
     nama = iNama;//definisi NIM dan Nama
 }
+
+int main(){
+    mahasiswa mhs1(12345);              //hanya NIM
+    mahasiswa mhs2("Rahmad");           //hanya nama
+    mahasiswa mhs3(12345, "Rahmad");    //hanya NIM dan Nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1 : "; mhs1.cetak();
+    cout << "Mahasiswa 2 : "; mhs2.cetak();
+    cout << "Mahasiswa 3 : "; mhs3.cetak();
+
+    return 0;
+}
